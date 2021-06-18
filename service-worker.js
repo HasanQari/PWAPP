8dfe44f4e30e45b36f80da033cef67f9ce78f894
HsQ-UQU <s437013480@st.uqu.edu.sa>
2021-06-18 03:47:50 +0300

    version 1.0.0

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
-
+self.addEventListener("fetch", function (event) {});
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
     new workbox.strategies.NetworkFirst()
